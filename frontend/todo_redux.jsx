9cd080584578bca79030d56ca0c366ad4435c3d2
Tony Hoyin Liu <tonyhoyinliu@gmail.com>
2016-09-15 12:09:28 -0700

    fixed package.json

--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+console.log("MADE IT");
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<h1>It worked!</h1>, root);
