9cce1f55a4fccddd3318eb3ee5fac52e5c8e58af
Tony Hoyin Liu <tonyhoyinliu@gmail.com>
2016-09-16 11:10:28 -0700

    todos actions created

--- frontend/actions/todo_actions.js
+++ frontend/actions/todo_actions.js
@@ -1,1 +1,15 @@
 // actions - POJOS
+// produce objects to be send through Middleware & to the store by invoking store#dispatch
+
+export const REQUEST_TODOS = "REQUEST_TODOS";
+export const RECEIVE_TODOS = "RECEIVE_TODOS";
+
+
+export const requestTodos = () => ({
+  type: REQUEST_TODOS
+});
+
+export const receiveTodos = todos => ({
+  type: RECEIVE_TODOS,
+  todos
+});
