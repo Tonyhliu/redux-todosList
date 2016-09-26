883f50b523b085dfa745d195eb8a04deabae4bdd
Tony Hoyin Liu <tonyhoyinliu@gmail.com>
2016-09-26 13:12:14 -0700

    continued middleware

--- frontend/middleware/todo_middleware.js
+++ frontend/middleware/todo_middleware.js
@@ -1,3 +1,12 @@
 import { REQUEST_TODOS, RECEIVE_TODOS } from '../actions/todo_actions';
+import RootReducer from '../reducers/root_reducer';
+import createLogger from 'redux-logger';
 
 // middleware set up
+// redux middleware receives dispatches before the store. It can
+// decide to intercept dispatch, trigger another dispatch, or pass
+// it along and do nothing
+
+const logger = createLogger();
+
+let store = createStore(RootReducer, applyMiddleware(logger));
